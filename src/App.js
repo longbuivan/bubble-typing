5ec90303ff0dc1057dfd08d52c4cc744b0ce9e23
longbuivan <longbuivan95@gmail.com>
2023-12-29 13:15:51 +0700

    Ver0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // CSS file to style the components
 
-function App() {
+const BubbleApp = () => {
+  const [displayText, setDisplayText] = useState('');
+
+  useEffect(() => {
+    let timeoutId;
+
+    const keyDownHandler = (e) => {
+      const typedChar = e.key;
+
+      if (
+        (e.keyCode === 8 || e.key === 'Backspace') && // Handling Backspace
+        displayText.length > 0
+      ) {
+        setDisplayText(prevText => prevText.slice(0, -1));
+      } else if (
+        e.key !== 'Shift' &&
+        e.key !== 'Enter' &&
+        /^[a-zA-Z0-9 .,!@]+$/.test(typedChar)
+      ) {
+        setDisplayText(prevText => {
+          const newText = prevText + typedChar;
+          return newText.length > 20 ? wrapText(newText) : newText;
+        });
+      }
+
+      if (e.key === 'Enter') {
+        setDisplayText('');
+      }
+
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setDisplayText('');
+      }, 2000); // Change the duration here for the bubble to disappear slowly
+    };
+
+    const wrapText = (text) => {
+      let wrappedText = '';
+      let remainingText = text;
+      while (remainingText.length > 100) {
+        let line = remainingText.substring(0, 100);
+        const lastSpaceIndex = line.lastIndexOf(' ');
+        if (lastSpaceIndex !== -1) {
+          line = line.substring(0, lastSpaceIndex);
+        }
+        wrappedText += line + '\n';
+        remainingText = remainingText.substring(line.length).trim();
+      }
+      if (remainingText.length > 0) {
+        wrappedText += remainingText;
+      }
+      return wrappedText;
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+      clearTimeout(timeoutId);
+    };
+  }, [displayText]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container" onClick={() => setDisplayText('')}>
+      <div className="bubble" style={{ display: displayText ? 'block' : 'none' }}>
+        {displayText}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default BubbleApp;
